741fa571c0a6226417d2848c5c8529e872682639
John Crisostomo <johncrisostomo@protonmail.ch>
2017-09-11 20:51:24 +0800

    deals with categorical data

--- data processing/data_processing_template.py
+++ data processing/data_processing_template.py
@@ -22,7 +22,10 @@ imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
-
-
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])
+X = onehotencoder.fit_transform(X).toarray()
 
 
